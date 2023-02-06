ae8d4a5271ba57a9f93f83883f70e37ab79edf58
alreuben <ayannahreuben@gmail.com>
2023-02-06 14:05:31 +0000

    pull req

--- functions.php
+++ functions.php
@@ -1,1 +1,14 @@
 <?php
+function createDbConnection(): PDO
+{
+    $db = new PDO('mysql:host=db; dbname=alr_games', 'root', 'password');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $db;
+}
+
+function retrieveGamesDb(PDO $db): array
+{
+    $stmt = $db->prepare("SELECT `name`, `year`, `developers`, `imdb_rating`, `image_url` FROM `games`");
+    $stmt->execute();
+    return $stmt->fetchAll();
+}
--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+require_once 'functions.php';
+
+$db = createDbConnection();
+$stmt = retrieveGamesDb();
+
+
+echo '<pre>';
+var_dump($stmt);
+echo '</pre>';
