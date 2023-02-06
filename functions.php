83d3bad41dd2e2b94974592edff2babc73a95185
alreuben <ayannahreuben@gmail.com>
2023-02-06 16:01:28 +0000

    display on page branch

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Creates a Db connection
+ *
+ * @return PDO the db connection
+ */
 function createDbConnection(): PDO
 {
     $db = new PDO('mysql:host=db; dbname=alr_games', 'root', 'password');
@@ -6,6 +11,11 @@ function createDbConnection(): PDO
     return $db;
 }
 
+/**
+ * retrieves all data from games Db
+ *
+ * @return array - all the games and stats
+ */
 function retrieveGamesDb(PDO $db): array
 {
     $stmt = $db->prepare("SELECT `name`, `year`, `developers`, `imdb_rating`, `image_url` FROM `games`");
--- index.php
+++ index.php
@@ -1,10 +1,15 @@
+
+<body>
 <?php
 require_once 'functions.php';
+include 'styles.css';
 
 $db = createDbConnection();
-$stmt = retrieveGamesDb();
+$stmt = retrieveGamesDb($db);
 
 
 echo '<pre>';
 var_dump($stmt);
 echo '</pre>';
+
+</html>
